c0cdb5362c7bf9bd88ee6a603ba0b4762c097f0e
Joás Campos <joascampos02@gmail.com>
2024-07-19 23:52:35 -0300

    Anatomia de classes 2

---
+++ src/MinhaClasse.java
@@ -0,0 +1,19 @@
+public class MinhaClasse {
+
+public static void main (String[] argus) {
+
+    String primeiroNome = "Joás";
+    String segundoNome = "Campos";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}  
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+
+}
+
+
+
+}
